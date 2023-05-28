8e8079e7b5141f0b2800fc931622ab3ee68f3b66
nizarabdouss <nizarabdouss@gmail.com>
2023-05-28 17:00:24 -0400

    Working on smart contract

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className="">
+      <h1> Hello World</h1>
+      <div className="">
+        <button className="bg-[#0000FF]">
+          
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
