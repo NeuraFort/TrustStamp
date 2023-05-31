c06df82c876370dc14ed48a5f4dd2729333be933
nizarabdouss <nizarabdouss@gmail.com>
2023-05-30 20:00:42 -0400

    Added Frontend folder and files

---
+++ client/Prismatica/src/App.jsx
@@ -0,0 +1,15 @@
+import { Footer, Main, Navbar, Search, Services, Transactions } from './Components';
+
+const App = () => {
+  
+
+  return (
+    <div className="min-h-screen">
+      <div>
+        <h1>Hello Wolrd</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ client/Prismatica/src/Components/index.js
@@ -0,0 +1,6 @@
+export { Footer } from './footer';
+export { Navbar } from './navbar';
+export { Footer } from './footer';
+export { Search } from './Search';
+export { Services } from './Services';
+export { Transactions } from './Transactions';
